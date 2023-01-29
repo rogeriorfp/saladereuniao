cf749acb4496d7c15bca76f314224563b4d200e6
Rogério de F. Pereira <rogeriorfp@outlook.com>
2023-01-29 03:02:31 -0300

    Cria RoomRepository

---
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.digital.crud.saladereuniao.saladereuniao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.digital.crud.saladereuniao.saladereuniao.model.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long>{
+    
+}
