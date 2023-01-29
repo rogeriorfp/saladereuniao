80c90601d8441c0e1d3f1ddca99d0a36e9860f22
Rogério de F. Pereira <rogeriorfp@outlook.com>
2023-01-29 02:34:33 -0300

    Configura modelo de entidade Room

---
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,27 @@
+package com.digital.crud.saladereuniao.saladereuniao.model;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "meetingroom")
+@Data
+public class Room {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private OffsetDateTime date;
+    @Column(nullable = false)
+    private OffsetDateTime startHour;
+    @Column(nullable = false)
+    private OffsetDateTime endHour;
+}
