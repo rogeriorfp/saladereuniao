10f31ea021c931177d2fbc5e9e150f6e3511ebae
Rogério de F. Pereira <rogeriorfp@outlook.com>
2023-02-04 20:58:10 -0300

    Adiciona documentação via Swagger(OpenAPI 3.0)

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
@@ -23,33 +23,44 @@ import com.digital.crud.saladereuniao.saladereuniao.exception.ResourceNotFoundEx
 import com.digital.crud.saladereuniao.saladereuniao.model.Room;
 import com.digital.crud.saladereuniao.saladereuniao.repository.RoomRepository;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 
 @RestController
 @RequestMapping("/api/v1/rooms")
 @CrossOrigin(origins = "http://localhost:4200")
+@Tag(name = "Rooms")
+@OpenAPIDefinition(info = @Info(title = "Meetings Room API", version = "1.0"))
 public class RoomController {
     
     @Autowired private RoomRepository roomRepository;
 
     @GetMapping
+    @Operation(summary = "List all rooms")
     public List<Room> getAllRooms() {
         return roomRepository.findAll();
     }
-
+    
     @GetMapping("/{roomId}")
+    @Operation(summary = "Retrieve room by id")
     public ResponseEntity<Room> getRoomById(@PathVariable long roomId) {
         Room room  = roomRepository.findById(roomId).orElseThrow(() -> new ResourceNotFoundException("Room not found: " + roomId));
         return ResponseEntity.ok(room);
     }
-
+    
     @PostMapping
+    @Operation(summary = "Create meeting room")
     @ResponseStatus(HttpStatus.CREATED)
     public Room createRoom(@Valid @RequestBody Room room) {
          return roomRepository.save(room);        
     }
 
     @PutMapping("/{roomId}")
+    @Operation(summary = "Update meeting room")
     public ResponseEntity<Room> updateRoom(@PathVariable Long roomId, @Valid @RequestBody Room room) {
         Room existingRoom = roomRepository.findById(roomId).orElseThrow(() -> new ResourceNotFoundException("Room not found for this id: " + roomId));
         BeanUtils.copyProperties(room, existingRoom, "id");
@@ -58,6 +69,7 @@ public class RoomController {
     }
 
     @DeleteMapping("/{roomId}")
+    @Operation(summary = "Delete meeting room")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteRoom(@PathVariable Long roomId) {
         Room room = roomRepository.findById(roomId).orElseThrow(() -> new ResourceNotFoundException("Room not found for this id: " + roomId));
